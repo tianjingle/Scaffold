7e81e3c06b1c9eae1cb43cd15f8797b4cd8cf5ee
tianjingle <2695062879@qq.com>
2020-09-24 09:14:10 +0800

    script

---
+++ src/main/java/com/inclination/scaffold/api/interfaces/GitMonitorApi.java
@@ -0,0 +1,16 @@
+package com.inclination.scaffold.api.interfaces;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @Author: tianjl
+ * @Date: 2020/6/24 14:50
+ * @Eamil: 2695062879@qq.com
+ */
+@RestController
+@Slf4j
+public class GitMonitorApi {
+
+
+}
---
+++ src/main/java/com/inclination/scaffold/application/monitor/GitMonitorService.java
@@ -0,0 +1,9 @@
+package com.inclination.scaffold.application.monitor;
+
+/**
+ * @Author: tianjl
+ * @Date: 2020/6/24 15:14
+ * @Eamil: 2695062879@qq.com
+ */
+public interface GitMonitorService {
+}
